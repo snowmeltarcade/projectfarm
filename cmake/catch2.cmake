872c93be250a881df68006d4f13167e20088e43d
Richard Hughes <richard.j.hughes@hotmail.com>
2021-05-22 16:42:36 -0400

    Find Catch2 with CMake.

--- cmake/catch2.cmake
+++ cmake/catch2.cmake
@@ -1,8 +1,8 @@
-add_subdirectory("${LIBRARIES_ROOT_DIR}/catch2")
+set(Catch2_DIR "${LIBRARIES_ROOT_DIR}/catch2/lib/cmake/Catch2" CACHE INTERNAL "Catch2_DIR")
 
 find_package(Catch2 REQUIRED)
 
 enable_testing()
 
 include(CTest)
-include("${LIBRARIES_ROOT_DIR}/catch2/contrib/Catch.cmake")
+include(Catch)
