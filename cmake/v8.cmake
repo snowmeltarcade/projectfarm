3b7f15fd9ff49b2def906c4db57af10fa5bae3e2
Richard Hughes <richard.j.hughes@hotmail.com>
2021-06-08 20:56:45 -0400

    Link against `iOS Simulator` libraries.

--- cmake/v8.cmake
+++ cmake/v8.cmake
@@ -2,6 +2,8 @@ set(V8_INCLUDE_DIRS "${LIBRARIES_ROOT_DIR}/v8/include" CACHE INTERNAL "V8_INCLUD
 
 if (WIN32)
     set(V8_LIBRARIES "${LIBRARIES_ROOT_DIR}/v8/Windows/v8_libbase.lib;${LIBRARIES_ROOT_DIR}/v8/Windows/v8_libplatform.lib;${LIBRARIES_ROOT_DIR}/v8/Windows/v8_monolith.lib;winmm.lib;dbghelp.lib" CACHE INTERNAL "V8_LIBRARIES")
+elseif (IOS)
+    set(V8_LIBRARIES "${LIBRARIES_ROOT_DIR}/v8/ios_simulator/libv8_libbase.a;${LIBRARIES_ROOT_DIR}/v8/ios_simulator/libv8_libplatform.a;${LIBRARIES_ROOT_DIR}/v8/ios_simulator/libv8_monolith.a" CACHE INTERNAL "V8_LIBRARIES")
 elseif (APPLE)
     set(V8_LIBRARIES "${LIBRARIES_ROOT_DIR}/v8/Darwin/libv8_libbase.a;${LIBRARIES_ROOT_DIR}/v8/Darwin/libv8_libplatform.a;${LIBRARIES_ROOT_DIR}/v8/Darwin/libv8_monolith.a" CACHE INTERNAL "V8_LIBRARIES")
 else()
