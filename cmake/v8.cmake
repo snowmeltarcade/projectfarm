060b8d965592c3560cfdf5bd8b9cb80fb15c0ad4
Richard Hughes <richard.j.hughes@hotmail.com>
2021-06-12 14:00:26 -0400

    Update v8 and SDL libs to include iOS builds.

--- cmake/v8.cmake
+++ cmake/v8.cmake
@@ -3,7 +3,8 @@ set(V8_INCLUDE_DIRS "${LIBRARIES_ROOT_DIR}/v8/include" CACHE INTERNAL "V8_INCLUD
 if (WIN32)
     set(V8_LIBRARIES "${LIBRARIES_ROOT_DIR}/v8/Windows/v8_libbase.lib;${LIBRARIES_ROOT_DIR}/v8/Windows/v8_libplatform.lib;${LIBRARIES_ROOT_DIR}/v8/Windows/v8_monolith.lib;winmm.lib;dbghelp.lib" CACHE INTERNAL "V8_LIBRARIES")
 elseif (IOS)
-    set(V8_LIBRARIES "${LIBRARIES_ROOT_DIR}/v8/ios_simulator/libv8_libbase.a;${LIBRARIES_ROOT_DIR}/v8/ios_simulator/libv8_libplatform.a;${LIBRARIES_ROOT_DIR}/v8/ios_simulator/libv8_monolith.a" CACHE INTERNAL "V8_LIBRARIES")
+    # we can include both the arm64 and x86_64 libs here as Xcode will choose the correct ones
+    set(V8_LIBRARIES "${LIBRARIES_ROOT_DIR}/v8/ios/libv8_libbase.a;${LIBRARIES_ROOT_DIR}/v8/ios/libv8_libplatform.a;${LIBRARIES_ROOT_DIR}/v8/ios/libv8_monolith.a;${LIBRARIES_ROOT_DIR}/v8/ios_simulator/libv8_libbase.a;${LIBRARIES_ROOT_DIR}/v8/ios_simulator/libv8_libplatform.a;${LIBRARIES_ROOT_DIR}/v8/ios_simulator/libv8_monolith.a" CACHE INTERNAL "V8_LIBRARIES")
 elseif (APPLE)
     set(V8_LIBRARIES "${LIBRARIES_ROOT_DIR}/v8/Darwin/libv8_libbase.a;${LIBRARIES_ROOT_DIR}/v8/Darwin/libv8_libplatform.a;${LIBRARIES_ROOT_DIR}/v8/Darwin/libv8_monolith.a" CACHE INTERNAL "V8_LIBRARIES")
 else()
